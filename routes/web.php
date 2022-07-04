4eaa375b89a7af3e08271a510cd88e73ae14caa2
Donnie Groves <donniegroves83@gmail.com>
2022-07-04 11:07:39 -0400

    created inside route

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/welcome', function(){
+Route::get('/', function(){
     return view('welcome');
 });
 
-Route::get('/', function () {
+Route::get('/inside', function () {
     return view('vue');
 });
 
