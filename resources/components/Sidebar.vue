74a2683e7d7d95b04aa38bc014c76eb6bc9704f2
Donnie Groves <donniegroves83@gmail.com>
2022-07-07 22:29:58 -0400

    Sidebar added

---
+++ resources/components/Sidebar.vue
@@ -0,0 +1,20 @@
+<template>
+    <header>
+        <h1>{{title}}<sup>2</sup></h1>
+    </header>
+</template>
+
+<script>
+    export default {
+        props: {
+            title: {
+                type: String,
+                default: 'GFin',
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,34 +1,21 @@
 <template>
-    <div class="container-fluid">
-        <div class="heading">
-            <h2 id="title">GFin (App.vue)</h2>
-        </div>
-        <transactions-list />
-        <transaction-add />
-    </div>
+    <Sidebar />
+    <TransactionsList />
+    <TransactionAdd />
 </template>
 
 <script>
 import TransactionsList from "./TransactionsList";
 import TransactionAdd from "./TransactionAdd";
+import Sidebar from '../components/Sidebar.vue';
 export default{
     components: {
         TransactionsList,
-        TransactionAdd
+        TransactionAdd,
+        Sidebar
     }
 }
 </script>
 
 <style scoped>
-.appContainer{
-    width: 650px;
-    margin: auto;
-}
-.heading{
-    background: #e6e6e6;
-    padding: 10px;
-}
-#title{
-    text-align: center;
-}
 </style>
