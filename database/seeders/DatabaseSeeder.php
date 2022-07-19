7f92a3e22d8879f9b9b4d2678ddfcc10d4088d33
Donnie Groves <donniegroves83@gmail.com>
2022-07-19 18:13:05 -0400

    categories table added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Seeder;
 use App\Models\Payee;
 use App\Models\PayeePattern;
 use App\Models\Transaction;
+use App\Models\Category;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +21,6 @@ class DatabaseSeeder extends Seeder
         Payee::factory(7)->create();
         PayeePattern::factory(3)->create();
         Transaction::factory(100)->create();
+        Category::factory(7)->create();
     }
 }
