8dd8d290ced662240af61b0999142c22df8b4c88
Donnie Groves <donniegroves83@gmail.com>
2022-12-27 22:03:21 -0500

    default timezone for scheduled tasks set to New York

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -8,6 +8,16 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 class Kernel extends ConsoleKernel
 {
     /**
+     * Get the timezone that should be used by default for scheduled events.
+     *
+     * @return \DateTimeZone|string|null
+     */
+    protected function scheduleTimezone()
+    {
+        return 'America/New_York';
+    }
+
+    /**
      * Define the application's command schedule.
      *
      * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
